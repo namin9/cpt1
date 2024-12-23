7e2f570f0ef75e9e498515d9efd57ff78ec3cca7
namin9 <96509320+namin9@users.noreply.github.com>
2024-12-24 01:43:15 +0900

    Update hash.js

--- js/hash.js
+++ js/hash.js
@@ -1,17 +1,20 @@
-// 해시에서 데이터를 읽기
-const hash = window.location.hash.substring(1);
-const params = new URLSearchParams(hash);
+document.addEventListener('DOMContentLoaded', () => {
+  // 해시에서 데이터를 읽기
+  const hash = window.location.hash.substring(1); // '#' 제거
+  const params = new URLSearchParams(hash);
 
-const features = params.get('features');
-const totalPrice = params.get('totalPrice');
+  const features = params.get('features');
+  const totalPrice = params.get('totalPrice');
 
-// 폼에 데이터 삽입
-if (features && totalPrice) {
-  const messageField = document.getElementById('message');
-  const defaultMessage = `
-    도입 문의 정보:
-    선택된 기능: ${features}
-    예상 총 요금: ${totalPrice}원
-  `;
-  messageField.value = defaultMessage.trim();
-}
+  // 폼에 데이터 삽입
+  if (features && totalPrice) {
+    const messageField = document.getElementById('message');
+    const defaultMessage = `
+      도입 문의
+      선택된 기능: ${features}
+      예상 총 요금: ${totalPrice}원
+      문의 내용 :
+    `;
+    messageField.value = defaultMessage.trim();
+  }
+});
